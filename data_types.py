4107baf0cea75e32eaf0d047856a04b7c0c9e04b
bibek2225op <bibek.giri2225@gmail.com>
2025-05-03 16:44:13 +0545

    data types with comments

---
+++ data_types.py
@@ -0,0 +1,22 @@
+a=10
+# this is integer data type which will contain value from negative infnity to positive infinity
+b=1.33
+# this is a float data type which will contain decimal values
+c="String"
+# this is a container that will have the string means we can write some letter
+d=True
+# this is a boolean data type that means its value will be either true or false
+fruit=["banana","apple","cherry"]
+# this is list data type means it can be changed we use mutable term for this
+coordinate=(2,3)
+# this is a tuple data types
+person={"name":"alice","age":"10"}
+# this is dictionary data type means  it has some values for the key name is key and alice is value
+print(a)
+print(b)
+print(c)
+print(d)
+print(fruit)
+print(coordinate)
+print(type(person))
+# this type(person) will show us which data type is that variable from example it will display person is dictionary data type
